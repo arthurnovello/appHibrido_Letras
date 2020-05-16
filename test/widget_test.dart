1501cf8c136e13152a2f0c302b337912ca808b8b
Arthur Novello <novelloarthur@gmail.com>
2020-05-16 10:47:35 -0300

    folders

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:mymusic/main.dart';
+import 'package:appHibrido_Letras/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
