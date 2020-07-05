c1011991d45237abbf84c8bc5675cf3054aa0c8d
Arthur Novello <arthurnovello@users.noreply.github.com>
2020-07-05 17:54:03 -0300

    homeScreen

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,24 +1,23 @@
+import 'package:appHibrido_Letras/screens/home/components/recomend.dart';
 import 'package:appHibrido_Letras/screens/home/components/title_with_more_button.dart';
 import 'package:flutter/material.dart';
-
-
 import 'header_with_searchbox.dart';
 
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery
-        .of(context)
-        .size;
+    Size size = MediaQuery.of(context).size;
     return SingleChildScrollView(
       child: Column(
         children: <Widget>[
           HeaderWithSearchBox(size, context),
-          TitleWithMoreButton(title: "Recommended", press: (){},),
-        ],),
+          TitleWithMoreButton(
+            title: "Recommended",
+            press: () {},
+          ),
+          Recomends(),
+        ],
+      ),
     );
   }
-
-
 }
-
